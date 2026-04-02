f09c8d01e58f240186013ddc45d840a3e5970ab9
Vivek-ray-05 <realvivek100@gmail.com>
2026-04-02 20:18:47 +0530

    implemented process creator and reactive ready queue UI

--- store/useSimulation.ts
+++ store/useSimulation.ts
@@ -1,32 +1,54 @@
 import { create } from 'zustand';
+import { Process } from '../.next/types/os';
 
 interface SimulationState {
+  // --- CLOCK STATE ---
   isPlaying: boolean;
   tick: number;
-  speed: number; 
+  speed: number;
   activeModule: string;
   
-  // Actions
+  // --- PROCESS STATE ---
+  processes: Process[]; 
+  
+  // --- ACTIONS ---
   togglePlay: () => void;
-  setSpeed: (newSpeed: number) => void;
   advanceTick: () => void;
-  resetSimulation: () => void;
+  setSpeed: (newSpeed: number) => void;
   setActiveModule: (module: string) => void;
+  addProcess: (p: Process) => void; 
+  resetSimulation: () => void;
 }
 
 export const useSimulation = create<SimulationState>((set) => ({
+  // Initial Values
   isPlaying: false,
   tick: 0,
   speed: 1,
   activeModule: 'cpu',
+  processes: [],
 
+  // Clock Actions
   togglePlay: () => set((state) => ({ isPlaying: !state.isPlaying })),
   
+  //advanceTick: () => set((state) => ({ tick: state.tick + 1 })), old v
+  advanceTick: () => set((state) => {
+  const newTick = state.tick + 1;
+   return { tick: newTick };
+}),
+
   setSpeed: (newSpeed) => set({ speed: newSpeed }),
-  
-  advanceTick: () => set((state) => ({ tick: state.tick + 1 })),
-  
-  resetSimulation: () => set({ tick: 0, isPlaying: false }),
 
   setActiveModule: (module) => set({ activeModule: module, tick: 0, isPlaying: false }),
+
+  // Process Actions
+  addProcess: (p) => set((state) => ({ 
+    processes: [...state.processes, p] 
+  })),
+  
+  resetSimulation: () => set({ 
+    tick: 0, 
+    isPlaying: false, 
+    processes: [] 
+  }),
 }));
